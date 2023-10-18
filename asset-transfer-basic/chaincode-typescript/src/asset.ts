075bc384c32b4949337820f5ac951ba21fa30d49
Benjamin <benjaoholeguy@gmail.com>
2023-10-18 22:35:44 +1300

    new structure

--- asset-transfer-basic/chaincode-typescript/src/asset.ts
+++ asset-transfer-basic/chaincode-typescript/src/asset.ts
@@ -10,27 +10,33 @@ export class Asset {
     @Property()
     public docType?: string;
 
-    // @Property()
-    // public ID: string;
+    @Property()
+    public product_id: string;
 
     @Property()
-    public VIN: string;
+    public supplier_id: string;
 
     @Property()
-    public Make: string;
+    public part_name: string;
 
     @Property()
-    public Model: string;
+    public shipment_date?: string;
 
     @Property()
-    public Year: number;
+    public expected_shipment_date: string;
 
     @Property()
-    public Owner: string;
+    public shipment_method_type: string;
 
     @Property()
-    public Accident?: number;
+    public longitude?: number;
 
     @Property()
-    public AppraisedValue: number;
+    public latitude?: number;
+
+    @Property()
+    public quantity?: number;
+
+    @Property()
+    public appraisedValue: number;
 }
--- asset-transfer-basic/chaincode-typescript/src/assetTransfer.ts
+++ asset-transfer-basic/chaincode-typescript/src/assetTransfer.ts
@@ -6,11 +6,6 @@ import {Context, Contract, Info, Returns, Transaction} from 'fabric-contract-api
 import stringify from 'json-stringify-deterministic';
 import sortKeysRecursive from 'sort-keys-recursive';
 import {Asset} from './asset';
-// import { TextEncoder } from 'util';
-
-// import {Oracle} from '../../application-oracle/src/application-oracle';
-// import {Oracle} from './application-oracle';
-
 
 @Info({title: 'AssetTransfer', description: 'Smart contract for trading assets'})
 export class AssetTransferContract extends Contract {
@@ -19,75 +14,100 @@ export class AssetTransferContract extends Contract {
     public async InitLedger(ctx: Context): Promise<void> {
         const assets: Asset[] = [
             {
-                VIN: "3D7JB1EP5BG913769",
-                Make: "Kia",
-                Model: "Rio",
-                Year: 2008,
-                Owner: "Rice and Sons",
-                AppraisedValue: 488685,
-                Accident: 3
+                product_id: "01HCZX3EZRQW1XWAM0DYMRW8RE",
+                supplier_id: "01HCZX3EZRH5J621GD8K2EJJEC",
+                part_name: "radiator",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-01-15",
+                shipment_method_type: "Ship",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 84.7
             },
             {
-                VIN: "KNDJT2A50C7413856",
-                Make: "Dodge",
-                Model: "Dakota Club",
-                Year: 1993,
-                Owner: "Larson-Marquardt",
-                AppraisedValue: 150607,
+                product_id: "01HCZXSSW4N1268V7GMYYV46TS",
+                supplier_id: "01HCZXSSW41229MJG7NCQAS43V",
+                part_name: "starter motor",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-08-25",
+                shipment_method_type: "Ship",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 29.22
             },
             {
-                VIN: "WVWBN7AN8FE740047",
-                Make: "Pontiac",
-                Model: "Aztek",
-                Year: 2002,
-                Owner: "Bailey and Sons",
-                AppraisedValue: 469869,
+                product_id: "01HCZXSSV9HZNB2F5Q1DEGTJ1W",
+                supplier_id: "01HCZXSSVAWH4ZSYYYXGR9XB3Y",
+                part_name: "exhaust manifold",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-08-30",
+                shipment_method_type: "Plane",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 55.0
             },
             {
-                // ID: 'asset1',
-                VIN: 'WP0AA2A94FS067403',
-                Make: 'Chrysler',
-                Model: 'Sebring',
-                Year: 1998,
-                Owner:"Rosenbaum-Cormier",
-                AppraisedValue: 499375,
+                product_id: "01HCZX3EZRQW1XWAM0DYMRW8RE",
+                supplier_id: "01HCZX3EZRH5J621GD8K2EJJEC",
+                part_name: "radiator",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2023-12-25",
+                shipment_method_type: "Ship",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 84.7
             },
             {
-                // ID: 'asset2',
-                VIN: '5NPDH4AE3DH363638',
-                Make: 'Toyota',
-                Model: 'Previa',
-                Year: 1992,
-                Owner:"Hudson Group",
-                AppraisedValue: 284879,
+                product_id: "01HCZXSSW8NSEEEWHT8H129XZM",
+                supplier_id: "01HCZXSSW90GQE3RAH91DVYVW0",
+                part_name: "shock absorber",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-08-14",
+                shipment_method_type: "Ship",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 53.97
             },
             {
-                // ID: 'asset3',
-                VIN: 'WAUBGBFC6CN394884',
-                Make: 'Buick',
-                Model: 'Enclave',
-                Year: 2009,
-                Owner:"Lind Group",
-                AppraisedValue: 352389,
+                product_id: "01HCZXSSWAR70S2N1GHNFZ2EEP",
+                supplier_id: "01HCZXSSWBB79CGWDSTR7HF4C5",
+                part_name: "serpentine belt",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-09-19",
+                shipment_method_type: "Plane",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 15.99
             },
             {
-                // ID: 'asset4',
-                VIN: '1N6AD0CU6CN312965',
-                Make: 'Pontiac',
-                Model: 'Grand Prix',
-                Year: 1990,
-                Owner:"Koepp Group",
-                AppraisedValue: 450676,
-                Accident: 3
+                product_id: "01HCZXSSWCWX8RY0M7XVGDKSR1",
+                supplier_id: "01HCZXSSWCYG669G59THR0QWP0",
+                part_name: "air filter",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-09-19",
+                shipment_method_type: "Truck",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 66.46
             },
             {
-                // ID: 'asset5',
-                VIN: 'WAUBC48H45K784418',
-                Make: 'Nissan',
-                Model: 'Xterra',
-                Year: 2005,
-                Owner:"Wunsch-Kihn",
-                AppraisedValue: 307097,
+                product_id: "01HCZXSSWJABQ6MW32SN8CZF2X",
+                supplier_id: "01HCZXSSWKJ0VG4D0AWW3AF52B",
+                part_name: "air filter",
+                // shipment_date: new Date(7,3,2024),
+                expected_shipment_date: "2024-07-30",
+                shipment_method_type: "Ship",
+                // longitude: -8.6708785,
+                // latitude: 39.6288756,
+                // quantity: 3,
+                appraisedValue: 22.94
             },
         ];
 
@@ -96,30 +116,34 @@ export class AssetTransferContract extends Contract {
             // example of how to write to world state deterministically
             // use convetion of alphabetic order
             // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-            // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
-            await ctx.stub.putState(asset.VIN, Buffer.from(stringify(sortKeysRecursive(asset))));
-            console.info(`Asset ${asset.VIN} initialized`);
+            // when retrieproduct_idg data, in any lang, the order of data will be the same and consequently also the corresonding hash
+            await ctx.stub.putState(asset.product_id, Buffer.from(stringify(sortKeysRecursive(asset))));
+            console.info(`Asset ${asset.product_id} initialized`);
         }
     }
 
     // CreateAsset issues a new asset to the world state with given details.
     @Transaction()
-    public async CreateAsset(ctx: Context, vin: string, make: string, model: string, year: number, owner: string, appraisedValue: number): Promise<void> {
-        const exists = await this.AssetExists(ctx, vin);
+    public async CreateAsset(ctx: Context, product_id: string, supplier_id: string, part_name: string, shipment_method_type: string, appraisedValue: number, shipment_date?: string, expected_shipment_date?: string, longitude?: number, latitude?: number, quantity?: number): Promise<void> {
+        const exists = await this.AssetExists(ctx, product_id);
         if (exists) {
-            throw new Error(`The asset ${vin} already exists`);
+            throw new Error(`The asset ${product_id} already exists`);
         }
 
         const asset = {
-            VIN: vin,
-            Make: make,
-            Model: model,
-            Year: year,
-            Owner: owner,
+            product_id: product_id,
+            supplier_id: supplier_id,
+            part_name: part_name,
+            shipment_date: (shipment_date != undefined) ? shipment_date : undefined,
+            expected_shipment_date: expected_shipment_date,
+            shipment_method_type: shipment_method_type,
+            longitude: (longitude != undefined) ? longitude : undefined,
+            latitude: (latitude != undefined) ? latitude : undefined,
+            quantity: (quantity != undefined) ? quantity : undefined,
             AppraisedValue: appraisedValue,
         };
         // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(vin, Buffer.from(stringify(sortKeysRecursive(asset))));
+        await ctx.stub.putState(product_id, Buffer.from(stringify(sortKeysRecursive(asset))));
     }
 
     // Check accident data for a particular vehicle
@@ -156,10 +180,10 @@ export class AssetTransferContract extends Contract {
 
     // ReadAsset returns the asset stored in the world state with given id.
     @Transaction(false)
-    public async ReadAsset(ctx: Context, id: string): Promise<string> {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+    public async ReadAsset(ctx: Context, product_id: string): Promise<string> {
+        const assetJSON = await ctx.stub.getState(product_id); // get the asset from chaincode state
         if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
+            throw new Error(`The asset ${product_id} does not exist`);
         }
 
         const assetString = assetJSON.toString()
@@ -168,67 +192,61 @@ export class AssetTransferContract extends Contract {
 
     // UpdateAsset updates an existing asset in the world state with provided parameters.
     @Transaction()
-    public async UpdateAsset(ctx: Context, vin: string, make: string, model: string, year: number, owner: string, appraisedValue: number, accident?: number): Promise<void> {
-        const exists = await this.AssetExists(ctx, vin);
+    public async UpdateAsset(ctx: Context, product_id: string, supplier_id: string, part_name: string, shipment_method_type: string, appraisedValue: number, shipment_date?: string, expected_shipment_date?: string, longitude?: number, latitude?: number, quantity?: number): Promise<void> {
+        const exists = await this.AssetExists(ctx, product_id);
         if (!exists) {
-            throw new Error(`The asset ${vin} does not exist`);
+            throw new Error(`The asset ${product_id} does not exist`);
         }
 
         // overwriting original asset with new asset
         const updatedAsset = {
-            VIN: vin,
-            Make: make,
-            Model: model,
-            Year: year,
-            Owner: owner,
+            product_id: product_id,
+            supplier_id: supplier_id,
+            part_name: part_name,
+            shipment_date: (shipment_date != undefined) ? shipment_date : undefined,
+            expected_shipment_date: expected_shipment_date,
+            shipment_method_type: shipment_method_type,
+            longitude: (longitude != undefined) ? longitude : undefined,
+            latitude: (latitude != undefined) ? latitude : undefined,
+            quantity: (quantity != undefined) ? quantity : undefined,
             AppraisedValue: appraisedValue,
         };
         // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(vin, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
+        return ctx.stub.putState(product_id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
     }
 
     // DeleteAsset deletes an given asset from the world state.
     @Transaction()
-    public async DeleteAsset(ctx: Context, id: string): Promise<void> {
-        const exists = await this.AssetExists(ctx, id);
+    public async DeleteAsset(ctx: Context, product_id: string): Promise<void> {
+        const exists = await this.AssetExists(ctx, product_id);
         if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+            throw new Error(`The asset ${product_id} does not exist`);
         }
-        return ctx.stub.deleteState(id);
+        return ctx.stub.deleteState(product_id);
     }
 
     // AssetExists returns true when asset with given ID exists in world state.
     @Transaction(false)
     @Returns('boolean')
-    public async AssetExists(ctx: Context, id: string): Promise<boolean> {
-        const assetJSON = await ctx.stub.getState(id);
+    public async AssetExists(ctx: Context, product_id: string): Promise<boolean> {
+        const assetJSON = await ctx.stub.getState(product_id);
         return assetJSON && assetJSON.length > 0;
     }
 
     // TransferAsset updates the owner field of asset with given id in the world state, and returns the old owner.
     @Transaction()
-    public async TransferAsset(ctx: Context, id: string, newOwner: string): Promise<string> {
-
-        const assetString = await this.ReadAsset(ctx, id);
+    public async TransferAsset(ctx: Context, product_id: string, quantity: string): Promise<string> {
+        const assetString = await this.ReadAsset(ctx, product_id);
         const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        
-        const done = await this.CallOracle(ctx, assetString, newOwner)
+        const done = await this.CallOracle(ctx, assetString, quantity)
                         .then(async (asset)=>{
-                            // await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
                             if (asset){
-                                return true
+                                return asset
                             }
                         });
         if(done){
-            return oldOwner;
+            return "ok";
         }
-        // if(asset.Accident==5){
-            // asset.AppraisedValue=asset.AppraisedValue*0.95
-        // }
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        // await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        // return oldOwner;
     }
 
     // GetAllAssets returns all assets found in the world state.
@@ -256,33 +274,33 @@ export class AssetTransferContract extends Contract {
 
     @Transaction(false)
     @Returns('string')
-    public async CallOracle(ctx: Context, assetString: string, newOwner: string): Promise<any> {
+    public async CallOracle(ctx: Context, assetString: string, quantity: string): Promise<any> {
         const asset = JSON.parse(assetString);
-        asset.Owner = newOwner;
-        const id = asset.VIN;
+        const id = asset.product_id;
 
         try {
             const apiKey = '795eb1c0';
             const request = async (id: string): Promise<Response> => {
-                const response = await fetch('https://my.api.mockaroo.com/accident.json', {
+                const response = await fetch('https://my.api.mockaroo.com/supply_chain.json', {
                     method: 'POST',
                     headers: {
                     'X-API-KEY': apiKey
                     },
-                    body: JSON.stringify({id})
+                    // body: JSON.stringify({id})
                 })
                 .then((response) => response.json())
                 .then(async (data)=>{
-                    asset.Accident = data.accident;
-                    if (data.accident == 5){
-                        asset.AppraisedValue=asset.AppraisedValue*0.95
-                    }
-                    await ctx.stub.putState(asset.VIN, Buffer.from(stringify(sortKeysRecursive(asset))));
+                    asset.shipment_date = data.shipment_date_str;
+                    // asset.longitude = data.part_location.longitude;
+                    // asset.latitude = data.part_location.latitude;
+                    asset.quantity = quantity;
+                    asset.appraisedValue = (new Date(data.shipment_date) > new Date(asset.expected_shipment_date)) ? asset.appraisedValue*0.95 : asset.appraisedValue*1.05;
+                    await ctx.stub.putState(asset.product_id, Buffer.from(stringify(sortKeysRecursive(asset))));
                     return data;
                 })
                 return response;
             };
-            const response = await request(asset.VIN);
+            const response = await request(asset.product_id);
         } catch (error) {
             console.log('*** Successfully caught the error: \n', error);
         } finally {
